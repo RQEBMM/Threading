616eca0555e03950ede9e01a7f522eeefac73985
Ben <r.q.einstein@gmail.com>
2013-03-12 19:22:25 -0400

    Initial Commit

---
+++ Threading/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Threading
+//
+//  Created by Benjamin McCloskey on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
